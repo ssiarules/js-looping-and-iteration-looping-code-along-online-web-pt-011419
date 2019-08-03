215afe89c61e71be5d36675351aad8de849065e6
Johnson Joseph <jhonsonj88@gmail.com>
2019-08-03 18:26:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // Code your solutions in this file
-function writeCards(["Johnson", "Joseph"], "birthday"){
 
+
+function writeCards(["Johnson", "Joseph"], "birthday"){
+    for (let i = 0, i < writeCards.length; i++) {
+      console.log( `Thank you, ${writeCards[i]}, for the wonderful birthday gift.,`);
+    }
 }
